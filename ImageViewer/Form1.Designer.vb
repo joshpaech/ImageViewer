fb116fbe3253d6f4a799c29027ac6a56754c9be0
Joshua Paech <joshpaech@gmail.com>
2015-08-05 11:35:35 +1000

    Completed code for all buttons

---
+++ ImageViewer/Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.picViewer = New System.Windows.Forms.PictureBox()
+        Me.chkStretch = New System.Windows.Forms.CheckBox()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.btnShow = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnBackground = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.TableLayoutPanel1.SuspendLayout()
+        CType(Me.picViewer, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.FlowLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.picViewer, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.chkStretch, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.FlowLayoutPanel1, 1, 1)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(590, 357)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'picViewer
+        '
+        Me.picViewer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.TableLayoutPanel1.SetColumnSpan(Me.picViewer, 2)
+        Me.picViewer.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.picViewer.Location = New System.Drawing.Point(3, 3)
+        Me.picViewer.Name = "picViewer"
+        Me.picViewer.Size = New System.Drawing.Size(584, 315)
+        Me.picViewer.TabIndex = 0
+        Me.picViewer.TabStop = False
+        '
+        'chkStretch
+        '
+        Me.chkStretch.AutoSize = True
+        Me.chkStretch.Location = New System.Drawing.Point(3, 324)
+        Me.chkStretch.Name = "chkStretch"
+        Me.chkStretch.Size = New System.Drawing.Size(60, 17)
+        Me.chkStretch.TabIndex = 1
+        Me.chkStretch.Text = "Stretch"
+        Me.chkStretch.UseVisualStyleBackColor = True
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnShow)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnClear)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnBackground)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnClose)
+        Me.FlowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(91, 324)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(496, 30)
+        Me.FlowLayoutPanel1.TabIndex = 2
+        '
+        'btnShow
+        '
+        Me.btnShow.AutoSize = True
+        Me.btnShow.Location = New System.Drawing.Point(3, 3)
+        Me.btnShow.Name = "btnShow"
+        Me.btnShow.Size = New System.Drawing.Size(88, 23)
+        Me.btnShow.TabIndex = 0
+        Me.btnShow.Text = "Show a picture"
+        Me.btnShow.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.AutoSize = True
+        Me.btnClear.Location = New System.Drawing.Point(97, 3)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(94, 23)
+        Me.btnClear.TabIndex = 1
+        Me.btnClear.Text = "Clear the picture"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnBackground
+        '
+        Me.btnBackground.AutoSize = True
+        Me.btnBackground.Location = New System.Drawing.Point(197, 3)
+        Me.btnBackground.Name = "btnBackground"
+        Me.btnBackground.Size = New System.Drawing.Size(143, 23)
+        Me.btnBackground.TabIndex = 2
+        Me.btnBackground.Text = "Set the background colour"
+        Me.btnBackground.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.AutoSize = True
+        Me.btnClose.Location = New System.Drawing.Point(346, 3)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 3
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All file" &
+    "s (*.*)|*.*"
+        Me.OpenFileDialog1.Title = "Select an image file"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(590, 357)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "Form1"
+        Me.Text = "Image Viewer"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        CType(Me.picViewer, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.FlowLayoutPanel1.ResumeLayout(False)
+        Me.FlowLayoutPanel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents picViewer As PictureBox
+    Friend WithEvents chkStretch As CheckBox
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents btnShow As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnBackground As Button
+    Friend WithEvents btnClose As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents ColorDialog1 As ColorDialog
+End Class
---
+++ ImageViewer/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+    Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
+
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            picViewer.Load(OpenFileDialog1.FileName)
+        End If
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        picViewer.Image = Nothing
+    End Sub
+
+    Private Sub btnBackground_Click(sender As Object, e As EventArgs) Handles btnBackground.Click
+        If ColorDialog1.ShowDialog() = DialogResult.OK Then
+            picViewer.BackColor = ColorDialog1.Color
+        End If
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Close()
+    End Sub
+
+    Private Sub chkStretch_CheckedChanged(sender As Object, e As EventArgs) Handles chkStretch.CheckedChanged
+        If chkStretch.Checked Then
+            picViewer.SizeMode = PictureBoxSizeMode.StretchImage
+        Else
+            picViewer.SizeMode = PictureBoxSizeMode.Normal
+        End If
+    End Sub
+End Class
